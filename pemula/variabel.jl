806e72a62296c8afa0b5f2ec4f4651e9a6383d61
mathcodegeek <nolduakali8@gmail.com>
2025-06-22 00:55:56 +0700

    Create variabel.jl

---
+++ pemula/variabel.jl
@@ -0,0 +1,94 @@
+###Nama :: Ari Ade Supriyatna
+###Whoami :: Researcher
+###WhatsApp :: https://wa.me/+6283899008328
+###Majenang
+###JawaTengah
+#&
+###BanjarPatroman
+###JawaBarat
+#&
+###Kawali, Bekasi, Bandung, Jakarta
+###indonesia
+#
+#
+#
+### SUMBER BELJAR ###
+# DeepSeek
+# docs.julialang.org/en/v1/manual/getting-started
+#
+### ALAT BANTU ###
+# Acode
+# colab.research.google.com
+# Redmi 8a Pro \ ram 2GB \ 32 bit armv7l
+#
+#
+#
+#
+#
+### opsi PERTAMA
+x = 50
+# variabel x bernilai 50
+a = x * 2
+# kemudian nilai variabel x di kali 2
+println("hasil: $a")
+# cetak string ke layar
+# 
+# 
+#
+#
+#
+### opsi KEDUA
+x = 50
+# versi singkatnya
+println("hasil: $(x * 2)")
+#
+#
+#
+#
+#
+### opsi KETIGA
+# blok `begin ... end` untuk mengelompokkan beberapa
+# ekspresi menjadi satu blok yang akan
+# dievaluasi secara berurutan,
+# karena `z + 50` adalah ekspresi terakhir, nilai
+# `100` akan disimpan ke dalam variabel `result`
+result = begin
+   x = 5
+   y = 10
+   z = x * y
+   z + 50
+end
+#
+#
+#
+#
+#
+### penulisan UNICODE
+# ^ adalah operator pangkat
+# π nilai pi
+# jadi artinya, berapa nilai pi berpangkat 2
+# gunakan apk UnicodePad dengan 03C0 untuk simbol unicode pi kecil ini, belajarlah karena sedikit banyak akan pakai unicode
+π ^ 2
+#
+#
+#
+#
+#
+### rubah ARRAY
+a = [10, 20, 30, 40]
+z = [20, 40, 80, 120]
+#
+# rubah elemen satu di array a
+# rubah elemen keempat di array z
+a[1] = 50
+z[4] = 100
+# index array dimulai dari 1 bukan dari 0
+#
+println(a)
+println(z)
+# array a akan menjadi [50, 20, 30, 40]
+# dan array z akan menjadi [20, 40, 80, 100]
+
+
+
+
