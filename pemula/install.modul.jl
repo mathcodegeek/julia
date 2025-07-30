fce5e58c26f45dfdb99c6c7eed034dff1e548883
ariadesupriyatna.medium.com <nolduakali8@gmail.com>
2025-07-30 15:22:25 +0700

    Update install.modul.jl

--- pemula/install.modul.jl
+++ pemula/install.modul.jl
@@ -1,2 +1,5 @@
 using Pkg
 Pkg.add("HTTP") #http adalah nama modulnya, kalian bebas tuh install modul apa aja sesuai kebutuhan
+
+
+#kunjungi https://juliapackages.com/ untuk melihat modul apa saja yang tersedia
